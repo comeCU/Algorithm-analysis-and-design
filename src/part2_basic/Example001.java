f8aa585d8546a96605232149511525eaea90949b
comeCU <1404842608@qq.com>
2018-09-17 21:21:36 +0800

    例1

---
+++ src/part2_basic/Example001.java
@@ -0,0 +1,43 @@
+package part2_basic;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @ClassName: Example001 
+ * @Description: link() 
+ * @author dong 
+ * @date 2018年9月17日 下午9:12:23 
+ * @keywords : 
+ *
+ */
+
+public class Example001 {
+	// 
+	public float getCumAndMul(int n) {
+		int i, sign;
+		float s, t = 1;
+//		Scanner input = new Scanner(System.in);
+//		n = input.nextInt();
+		s = 1;
+		sign = 1;
+		for(i = 2; i<= n; i++) {
+			sign = -sign;
+			t = t*(2 * i - 2) * (2 * i - 1);
+			s = s + sign / t;
+		}
+		return s;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("请输入：");
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		
+		Example001 ex001 = new Example001();
+		System.out.println(ex001.getCumAndMul(n));
+	}
+
+}
