1e560b32a6b3dace608c84e47880f787273d5d2b
comeCU <1404842608@qq.com>
2018-09-19 22:31:00 +0800

    例6 so hard

---
+++ src/part2_basic/Example006.java
@@ -0,0 +1,19 @@
+package part2_basic;
+
+/**
+ * 
+ * @ClassName: Example006 
+ * @Description: 
+ * @author dong 
+ * @date Sep 19, 2018 10:25:51 PM 
+ * @keywords : 正整数分划
+ *
+ */
+public class Example006 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
