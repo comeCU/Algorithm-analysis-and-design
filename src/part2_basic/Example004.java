f938666f0826f7ecb5babb632a26561430c14110
comeCU <1404842608@qq.com>
2018-09-21 15:33:44 +0800

    例4

---
+++ src/part2_basic/Example004.java
@@ -0,0 +1,45 @@
+package part2_basic;
+
+public class Example004 {
+	
+	/**
+	 * 用数组存储数字图形
+	 * @param n n行n列
+	 * @return arr二维数组
+	 */
+	public int[][] numStore(int n) {
+		int[][] arr = new int[n][n];
+		int k = 1;
+		for(int i = 0; i< n; i++) {
+			for(int j = 0; j< n - i; j++) {	// 按对角线规律存储
+				arr[i+j][j] = k;
+				k++;
+			}
+		}
+		return arr;
+	}
+	
+	/**
+	 * 输出
+	 * @param arr
+	 */
+	public void printNumStore(int[][] arr) {
+		for(int i = 0; i< arr.length; i++) {
+			for(int j = 0; j< i + 1; j++) {	// 只输出存储了有效数字的数据
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Example004 ex004 = new Example004();
+		int[][] arr = ex004.numStore(6);
+		
+		ex004.printNumStore(arr);
+		
+	}
+
+}
