701d414698b295e71421ba459446708b4ba8cb1c
comeCU <1404842608@qq.com>
2018-09-22 21:30:01 +0800

    例5

--- src/part2_basic/Example005.java
+++ src/part2_basic/Example005.java
@@ -1,24 +1,38 @@
 package part2_basic;
 
+/**
+ * 
+ * @ClassName: Example005
+ * @Description: 汉诺塔问题
+ * @author dong
+ * @date Sep 22, 2018 9:24:45 PM
+ * @keywords :
+ *
+ */
 public class Example005 {
-	/**
-	 * 汉诺塔问题
-	 * @param n 问题阶数
-	 * @param a 塔A
-	 * @param b 塔B
-	 * @param c 塔C
-	 */
-	public void hanoi(int n, char a, char b, char c) {
-		if(n > 0) {
-			hanoi(n - 1, a, c, b);
-			System.out.println("move dish " + n + " : " + a + " -> " + b);
-			hanoi(n - 1, c, b, a);
-		}
-	}
-	
-	public static void main(String[] args) {
-		Example005 ex005 = new Example005();
-		ex005.hanoi(3, 'A', 'B', 'C');
-	}
+    /**
+     * 汉诺塔问题
+     * 
+     * @param n
+     *            问题阶数
+     * @param a
+     *            塔A
+     * @param b
+     *            塔B
+     * @param c
+     *            塔C
+     */
+    public void hanoi(int n, char a, char b, char c) {
+        if (n > 0) {
+            hanoi(n - 1, a, c, b);
+            System.out.println("move dish " + n + " : " + a + " -> " + b);
+            hanoi(n - 1, c, b, a);
+        }
+    }
+
+    public static void main(String[] args) {
+        Example005 ex005 = new Example005();
+        ex005.hanoi(3, 'A', 'B', 'C');
+    }
 
 }
