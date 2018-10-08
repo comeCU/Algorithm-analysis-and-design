c000687c9a22b47002b4ca591ea2793bf5ba5620
comeCU <1404842608@qq.com>
2018-10-08 18:31:27 +0800

    例18 version 2.0

--- src/part2_basic/Example018.java
+++ src/part2_basic/Example018.java
@@ -12,9 +12,10 @@ public class Example018 {
 
     /**
      * 高精度数据 * 长整数，并将结果存储在数组中
-     * @param s1 存储高精度数据的字符串数组
+     * @param s1 存储高精度数据的字符数组
      * @param num 长整型数
      * @return 返回结果数组
+     * @version 1.0
      */
     public int[] saveLargeInteger(char[] s1, long num) {
         long bitOfProduct = 0;   // 高精度数据保存在字符数组中每一位的乘积
@@ -31,6 +32,33 @@ public class Example018 {
             save[len] = carry % 10;
             carry /= 10;
             len++;
+        }
+        return save;
+    }
+     
+    /**
+     * 高精度数据 * 长整数，并将结果存储在数组中
+     * @param str 存储高精度数据的字符串数组
+     * @param num 长整型数
+     * @return 返回结果数组
+     * @version 2.0
+     */
+    public int[] saveLargeInteger2(String str, long num) {
+        long bitOfProduct = 0;
+        int carry = 0;  // 进位
+        int len = str.length();
+        int[] save = new int[256];
+        
+        int i = 0;
+        int j = len - 1;
+        for(; j >= 0; j--, i++) {
+            bitOfProduct = (str.charAt(j) - 48) * num + carry;
+            save[i] = (int) (bitOfProduct % 10);
+            carry = (int) (bitOfProduct / 10);
+        }
+        while(carry != 0) {
+            save[len] = carry % 10;
+            carry /= 10;
         }
         return save;
     }
@@ -56,10 +84,13 @@ public class Example018 {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         Example018 ex18 = new Example018();
-        char[] s1 = {'3', '6', '9', '1', '2', '3', '3', '4', '5', '9', '6', '7', '4', '6', '5'};
+        /*char[] s1 = {'3', '6', '9', '1', '2', '3', '3', '4', '5', '9', '6', '7', '4', '6', '5'};
         int[] arr = ex18.saveLargeInteger(s1, 6);
-        ex18.printSaveLargeInteger(arr);
+        ex18.printSaveLargeInteger(arr);*/
         
+        System.out.println();
+        int[] arr2 = ex18.saveLargeInteger2("369123345967465", 6);
+        ex18.printSaveLargeInteger(arr2);
     }
 
 }
